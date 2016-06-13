b5bbe8a616f3679f358462696fb778906654fc95
Renaud11232 <gaspardrenaud@hotmail.com>
2016-06-13 03:08:09 +0200

    optimized imports, reformatted code & added MIT licence headers

--- src/be/renaud11232/streamscraperreloaded/parser/ShoutCast1StatusParser.java
+++ src/be/renaud11232/streamscraperreloaded/parser/ShoutCast1StatusParser.java
@@ -1,3 +1,13 @@
+/*
+ * Copyright (c) 2016 R. Gaspard
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package be.renaud11232.streamscraperreloaded.parser;
 
 import be.renaud11232.streamscraperreloaded.ParseException;
@@ -11,12 +21,12 @@ import java.net.URI;
 import java.util.Collections;
 import java.util.List;
 
-public class ShoutCast1StatusParser implements Parser{
+public class ShoutCast1StatusParser implements Parser {
     @Override
     public List<Stream> parse(URI uri, Document src) throws ParseException {
         try {
             return parseSource(uri, src);
-        } catch (Exception e){
+        } catch (Exception e) {
             throw new ParseException(e);
         }
     }
@@ -25,8 +35,8 @@ public class ShoutCast1StatusParser implements Parser{
         Stream stream = new Stream();
         stream.setUri(uri.resolve("/"));
         Elements tables = src.select("table[align=center]");
-        for(Element table : tables){
-            if(parseTable(table, stream)){
+        for (Element table : tables) {
+            if (parseTable(table, stream)) {
                 return Collections.singletonList(stream);
             }
             stream.clear();
@@ -34,12 +44,12 @@ public class ShoutCast1StatusParser implements Parser{
         return Collections.emptyList();
     }
 
-    private boolean parseTable(Element table, Stream stream){
+    private boolean parseTable(Element table, Stream stream) {
         boolean toReturn = false;
         Elements rows = table.select(":root > tbody > tr");
-        for(Element row : rows){
+        for (Element row : rows) {
             Elements cols = row.select(":root > td");
-            if(cols.size() == 2){
+            if (cols.size() == 2) {
                 String name = cols.get(0).text().trim();
                 String value = cols.get(1).text();
                 toReturn = parseAttribute(name, value, stream) || toReturn;
@@ -48,8 +58,8 @@ public class ShoutCast1StatusParser implements Parser{
         return toReturn;
     }
 
-    private boolean parseAttribute(String name, String value, Stream stream){
-        switch(name.toLowerCase()){
+    private boolean parseAttribute(String name, String value, Stream stream) {
+        switch (name.toLowerCase()) {
             case "listener peak:":
                 stream.setPeakListenerCount(Integer.parseInt(value));
                 return true;
--- src/be/renaud11232/streamscraperreloaded/scraper/ShoutCast2Scraper.java
+++ src/be/renaud11232/streamscraperreloaded/scraper/ShoutCast2Scraper.java
@@ -1,3 +1,13 @@
+/*
+ * Copyright (c) 2016 R. Gaspard
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package be.renaud11232.streamscraperreloaded.scraper;
 
 import be.renaud11232.streamscraperreloaded.FetchException;
@@ -14,12 +24,12 @@ import java.net.URI;
 import java.util.LinkedList;
 import java.util.List;
 
-public class ShoutCast2Scraper implements Scraper{
+public class ShoutCast2Scraper implements Scraper {
 
     private HttpFetcher fetcher;
     private ShoutCast2StatusParser parser;
 
-    public ShoutCast2Scraper(){
+    public ShoutCast2Scraper() {
         fetcher = new HttpFetcher();
         parser = new ShoutCast2StatusParser();
     }
@@ -30,7 +40,7 @@ public class ShoutCast2Scraper implements Scraper{
             List<Stream> streams = new LinkedList<>();
             List<Document> stats = getAvailableStreams(uri);
             int i = 1;
-            for(Document stat : stats){
+            for (Document stat : stats) {
                 streams.addAll(parser.parse(uri.resolve("/index.html?sid=" + i), stat));
                 i++;
             }
@@ -43,8 +53,8 @@ public class ShoutCast2Scraper implements Scraper{
     private List<Document> getAvailableStreams(URI uri) throws FetchException {
         Document stats = fetcher.fetch(uri.resolve("/index.html?sid=0"));
         Elements streamLinks = stats.select("td.tnl > a:first-of-type");
-        List <Document> list = new LinkedList<>();
-        for(Element link : streamLinks){
+        List<Document> list = new LinkedList<>();
+        for (Element link : streamLinks) {
             String id = link.attr("href").split("sid=")[1];
             list.add(fetcher.fetch(uri.resolve("/stats?sid=" + id)));
         }
